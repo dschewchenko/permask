f4b5500eb4abfdc2c2f2670da66cbd433baa334a
productdevbook <hi@productdevbook.com>
2025-04-14 11:03:06 +0300

    feat: Update GitHub Actions workflow for Vue Playground deployment and remove unnecessary 404 page

--- playground/vue/vite.config.ts
+++ playground/vue/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/permask-fork/' : '/',
+  base: '/permask-fork/',
   plugins: [
     vue(),
     tailwindcss()
