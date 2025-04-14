51194a53a58d60203df681a181f58434b2a8b91f
productdevbook <hi@productdevbook.com>
2025-04-14 10:50:44 +0300

    feat: Add GitHub Pages deployment workflow and enhance SPA redirection in HTML files

--- playground/vue/vite.config.ts
+++ playground/vue/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/permask-fork/' : '/',
   plugins: [
     vue(),
     tailwindcss()
